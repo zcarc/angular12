bd4adeffebda1a41551ac34ad6de7535c9054684
Hyunsoo Lee <wrkbrs@gmail.com>
2021-05-26 21:24:08 +0900

    목록 표시하기

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
 import { Hero } from './hero';
 
 @Component({
@@ -7,10 +8,13 @@ import { Hero } from './hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() {}
 
